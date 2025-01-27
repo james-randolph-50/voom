c18531e0a09b0646e0ac7a5ef7b3bddbd8441d1c
James Randolph <james.randolph2016@gmail.com>
2025-01-27 12:31:07 -0500

    add clerk authentication

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import MobileNav from './MobileNav';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 
 const Navbar = () => {
@@ -21,7 +22,11 @@ const Navbar = () => {
     </Link>
 
     <div className="flex-between gap-5">
+        <SignedIn>
+              <UserButton />
+        </SignedIn>
 
+        
         <MobileNav />
       </div>
     </nav>

16ff0c6c9130cbfcee1f3b925b890ea1ad22c211
James Randolph <james.randolph2016@gmail.com>
2025-01-27 10:47:21 -0500

    render links and icons in sidebar

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,11 +1,14 @@
 import React, { ReactNode } from 'react'
+import Navbar from '@/components/ui/Navbar'
+import Sidebar from '@/components/ui/Sidebar'
+
 
 const HomeLayout = ({children}:{children:ReactNode}) => {
   return (
     <main className='relative'>
-        NavBar
+        <Navbar />
         <div className='flex'>
-            Sidebar
+            <Sidebar />
             <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
                 <div className='w-full'>
                     {children}

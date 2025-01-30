7e86626435bae69d3b8ca599f324a5c5bb9d9215
James Randolph <james.randolph2016@gmail.com>
2025-01-30 09:29:20 -0500

    add join meeting functionality

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -7,12 +7,10 @@ import MeetingModal from './MeetingModal'
 import { useUser } from '@clerk/nextjs'
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import Loader from './Loader'
-import { Toast } from './ui/toast'
 import { useToast } from '@/hooks/use-toast'
 import { Textarea } from "@/components/ui/textarea"
 import ReactDatePicker from 'react-datepicker';
-
-
+import { Input } from "@/components/ui/input"
 
 const initialValues = {
   dateTime: new Date(),
@@ -156,6 +154,21 @@ const MeetingTypeList = () => {
         buttonText="Start Meeting"
         handleClick={createMeeting}
       />
+      <MeetingModal
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Type link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}
+      >
+
+        <Input 
+          placeholder='Meeting link'
+          className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0'
+          onChange={(e) => setValues({...values, link: e.target.value })}
+        />
+      </MeetingModal>
     </section>
   )
 }

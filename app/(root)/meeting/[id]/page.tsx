baf858554a816b115e62dd76ad2210fa6c4eda45
James Randolph <james.randolph2016@gmail.com>
2025-01-30 09:57:49 -0500

    remove not allwoed

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -26,7 +26,7 @@ const MeetingPage = () => {
   );
 
   // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
-  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+  // const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
 
 
 
